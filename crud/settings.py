ea8651b3b80eb7d54eefd971f524b67f885a054f
Tejas Nirmal <102731333+Tejasnirmal123@users.noreply.github.com>
2024-01-25 13:18:06 +0530

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': '5gD6CcFgdafeabCcBA*EB*BD3GB3gace',
