0fa01a01c690945821f2d5a7100bebfcf7c1d418
Tejas Nirmal <102731333+Tejasnirmal123@users.noreply.github.com>
2024-01-25 13:25:26 +0530

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -132,6 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [‘https://crudoperations-production.up.railway.app ’]
 
 
 
