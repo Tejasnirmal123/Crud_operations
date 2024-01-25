934f7547815644c9dc378d93ef3ec8dd390ed9d7
Tejas Nirmal <102731333+Tejasnirmal123@users.noreply.github.com>
2024-01-25 12:19:51 +0530

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-imbx+qmy0x@)8u&i(7sb%lsee)4%9ep@$@^s40%8%8e=zlujwv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["crudoperations-production.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -132,4 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+‘https://*.railway.app’
+]
 
