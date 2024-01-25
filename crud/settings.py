97b32532579c3f5710bed9a6d2de526ffe09f88a
Tejas Nirmal <102731333+Tejasnirmal123@users.noreply.github.com>
2024-01-25 12:56:46 +0530

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-imbx+qmy0x@)8u&i(7sb%lsee)4%9ep@$@^s40%8%8e=zlujwv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["crudoperations-production.up.railway.app", "localhost",]
 
